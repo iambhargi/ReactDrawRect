0d2f4f0d86d55016612ddb69480f5dffd9f5a5bb
Bhargav Patel <iambhargi@gmail.com>
2020-09-21 21:09:35 +0530

    Updates

---
+++ precache-manifest.f610f941ddf7254b93119149fbb8d8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ccaa549cc6f4d2b4896bd7727532fec",
+    "url": "/ReactDrawRect/index.html"
+  },
+  {
+    "revision": "4ed382662925658bb2b6",
+    "url": "/ReactDrawRect/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c66b42304db039563434",
+    "url": "/ReactDrawRect/static/js/2.8d1abb01.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/ReactDrawRect/static/js/2.8d1abb01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed382662925658bb2b6",
+    "url": "/ReactDrawRect/static/js/main.a83880ae.chunk.js"
+  },
+  {
+    "revision": "a1755809adc87f437aa6",
+    "url": "/ReactDrawRect/static/js/runtime-main.45077511.js"
+  },
+  {
+    "revision": "aaf28585e54dd61f1fcd0c82fcf6266d",
+    "url": "/ReactDrawRect/static/media/bgImage.aaf28585.png"
+  }
+]);
